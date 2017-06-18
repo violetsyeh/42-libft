0794e144ab2ce02e8686f43508528cf945a8a2f1
Violet Yeh <violetyeh@violets-air.attlocal.net>
2017-06-18 16:16:57 -0700

    edits and new additions

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: violetyeh <violetyeh@student.42.fr>        +#+  +:+       +#+        */
+/*   By: vyeh <vyeh@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/06/05 16:09:54 by violetyeh         #+#    #+#             */
-/*   Updated: 2017/06/05 17:31:07 by violetyeh        ###   ########.fr       */
+/*   Created: 2017/06/13 17:15:31 by vyeh              #+#    #+#             */
+/*   Updated: 2017/06/18 16:14:57 by vyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ char	*ft_strdup(const char *s1)
 	s2 = (char*)malloc((ft_strlen(s1) + 1) * (sizeof(char)));
 	if (s2 == NULL)
 		return(NULL);
-	return (ft_strcpy(s2, s1)); 
+	return (ft_strcpy(s2, s1));
 }
