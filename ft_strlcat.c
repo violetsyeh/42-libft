da9cf8890252a771cf9e92f12366587b4ec2636f
Violet Yeh <violetyeh@violets-air.attlocal.net>
2017-06-24 17:38:12 -0700

    strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vyeh <vyeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/23 16:27:51 by vyeh              #+#    #+#             */
+/*   Updated: 2017/06/24 17:37:45 by vyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	len;
+	size_t	i;
+
+	i = 0;
+	while (dst[i] && i < size)
+		i++;
+	len = i;
+	while (src[i - len] && i < size - 1)
+	{
+		dst[i] = src[i - len];
+		i++;
+	}
+	if (len < size)
+		dst[i] = '\0';
+	return (len + ft_strlen(src));
+}
