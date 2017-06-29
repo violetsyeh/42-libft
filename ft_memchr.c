f9d71f8b8de105689054bfed6b8839c25de91bf9
Violet Yeh <violetyeh@Violets-MacBook-Air.local>
2017-06-29 14:12:37 -0700

    memchr/memcmp

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vyeh <vyeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/28 19:32:51 by vyeh              #+#    #+#             */
+/*   Updated: 2017/06/28 19:47:05 by vyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+	char	*str;
+	size_t	counter;
+
+	if (!(str = (char *)s))
+		return (NULL);
+	counter = 0;
+	while (counter < n)
+	{
+		if ((char)c == str[0])
+			return ((void*)str);
+		str++;
+		counter++;
+	}
+	return (NULL);
+}
---
+++ ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vyeh <vyeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/28 18:46:30 by vyeh              #+#    #+#             */
+/*   Updated: 2017/06/28 19:32:48 by vyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	char	*p;
+	char	*d;
+
+	i = 0;
+	p = (char *)s1;
+	d = (char *)s2;
+	while (i < n)
+	{
+		if (*p != *d)
+			return ((unsigned char)*p - (unsigned char)*d);
+		i++;
+		p++;
+		d++;
+	}
+	return (0);
+}
