ebc744ca8d5df19c0e1816c8a63134e74d4346d0
Violet Yeh <violetyeh@Violets-MacBook-Air.local>
2017-06-29 14:29:04 -0700

    atoi/memccpy

---
+++ ft_memccpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vyeh <vyeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/29 14:18:50 by vyeh              #+#    #+#             */
+/*   Updated: 2017/06/29 14:28:46 by vyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		*((unsigned char *)dst + i) = *((unsigned char *)src + i);
+		if (*((unsigned char *)src + i) == (unsigned char)c)
+			return ((unsigned char *)dst + i + 1);
+		i++;
+	}
+	return (NULL);
+}
