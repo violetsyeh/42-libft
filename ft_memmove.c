5ccead554e25eabeea8113f4cff25c05f5d4b3d9
Violet Yeh <violetyeh@Violets-MacBook-Air.local>
2017-06-28 18:46:11 -0700

    memcpy/memmove

---
+++ ft_memmove.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vyeh <vyeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/28 17:18:25 by vyeh              #+#    #+#             */
+/*   Updated: 2017/06/28 18:45:51 by vyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t			i;
+	unsigned char	*p;
+	unsigned char	*b;
+
+	i = 0;
+	p = (unsigned char *)dst;
+	b = (unsigned char *)src;
+	if (dst < src)
+		ft_memcpy(dst, src, len);
+	else
+	{
+		while (len--)
+		{
+			p[len] = b[len];
+		}
+	}
+	return (dst);
+}
