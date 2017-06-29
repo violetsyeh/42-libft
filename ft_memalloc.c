51b4b458fa74d31664766146492861c0372a7942
Violet Yeh <violetyeh@Violets-MacBook-Air.local>
2017-06-29 15:34:45 -0700

    memalloc

---
+++ ft_memalloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vyeh <vyeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/29 15:29:11 by vyeh              #+#    #+#             */
+/*   Updated: 2017/06/29 15:29:14 by vyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	*ft_memalloc(size_t size)
+{
+	void *mem;
+
+	if ((mem = (void*)malloc(size)) && size)
+	{
+		ft_bzero(mem, size);
+		return (mem);
+	}
+	return (NULL);
+}
