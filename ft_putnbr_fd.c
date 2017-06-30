f61f796a51d4c14ea968a17d443e13901ec3f1f7
Violet Yeh <violetyeh@Violets-MacBook-Air.local>
2017-06-29 17:44:55 -0700

    putnbr

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vyeh <vyeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/29 17:41:47 by vyeh              #+#    #+#             */
+/*   Updated: 2017/06/29 17:44:31 by vyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	nb;
+
+	if (n < 0)
+	{
+		nb = -n;
+		ft_putchar_fd('-', fd);
+	}
+	else
+		nb = n;
+	if (nb >= 10)
+	{
+		ft_putnbr_fd((nb / 10), fd);
+		ft_putchar_fd(((nb % 10) + '0'), fd);
+	}
+	else
+	{
+		if (n > 0)
+			ft_putchar_fd(('0' + n), fd);
+		else
+			ft_putchar_fd(('0' - n), fd);
+	}
+}
