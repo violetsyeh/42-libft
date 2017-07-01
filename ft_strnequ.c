19d1578d82f65be3f286cb0519e5259062a2bee9
Violet Yeh <violetyeh@Violets-MacBook-Air.local>
2017-07-01 15:06:59 -0700

    strnequ

---
+++ ft_strnequ.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnequ.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vyeh <vyeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/29 18:43:33 by vyeh              #+#    #+#             */
+/*   Updated: 2017/07/01 15:03:28 by vyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strnequ(char const *s1, char const *s2, size_t n)
+{
+	char *str1;
+	char *str2;
+
+	str1 = (char *)s1;
+	str2 = (char *)s2;
+	if (str1 && str2)
+	{
+		if (ft_strncmp(str1, str2, n) == 0)
+			return (1);
+		else
+			return (0);
+	}
+	return (0);
+}
