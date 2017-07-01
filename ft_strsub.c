c7490bcc71e6191601c4e7dfffb756f6cc6a6649
Violet Yeh <violetyeh@Violets-MacBook-Air.local>
2017-07-01 16:05:12 -0700

    strsub/strtrim

---
+++ ft_strsub.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vyeh <vyeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/01 15:12:27 by vyeh              #+#    #+#             */
+/*   Updated: 2017/07/01 15:21:43 by vyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char			*str;
+	char			*str_new;
+	unsigned int	counter;
+
+	if (!(str_new = ft_strnew(len)) || !s)
+		return (NULL);
+	str = (char *)s;
+	counter = 0;
+	while (counter < len)
+	{
+		str_new[counter] = str[start];
+		start++;
+		counter++;
+	}
+	return (str_new);
+}
---
+++ ft_strtrim.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vyeh <vyeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/01 15:51:09 by vyeh              #+#    #+#             */
+/*   Updated: 2017/07/01 16:04:20 by vyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s)
+{
+	size_t	i;
+	size_t	l;
+	char	*temp;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	l = 0;
+	while (s[i] && (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'))
+		i++;
+	l = ft_strlen(&s[i]) - 1;
+	while (s[i] && (s[l + i] == ' ' || s[l + i] == '\t' || s[l + i] == '\n'))
+		l--;
+	temp = ft_strnew(l + 1);
+	if (!temp)
+		return (NULL);
+	ft_strncpy(temp, s + i, l + 1);
+	return (temp);
+}
