cecce590d315906c7d8f83a2c884c129f6937433
Violet Yeh <violetyeh@Violets-MacBook-Air.local>
2017-07-02 18:42:28 -0700

    libft

---
+++ ft_lstmap.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vyeh <vyeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/02 18:36:52 by vyeh              #+#    #+#             */
+/*   Updated: 2017/07/02 18:40:27 by vyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list *new_lst;
+	t_list *new_elem;
+	t_list *prev_elem;
+
+	new_lst = NULL;
+	if (lst && (*f))
+	{
+		new_lst = (*f)(lst);
+		prev_elem = new_lst;
+		lst = lst->next;
+		while (lst)
+		{
+			new_elem = (*f)(lst);
+			prev_elem->next = new_elem;
+			prev_elem = new_elem;
+			lst = lst->next;
+		}
+		prev_elem->next = NULL;
+	}
+	return (new_lst);
+}
